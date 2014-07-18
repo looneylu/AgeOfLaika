e3edf2b92738315e712eed7c1dc8a070b57e17fd
looneylu <lcarbuccia@gmail.com>
2014-07-18 02:23:49 -0400

    more accurate

--- AgeOfLaika/AgeOfLaika/LRCViewController.m
+++ AgeOfLaika/AgeOfLaika/LRCViewController.m
@@ -19,8 +19,17 @@
 
 - (IBAction)buttonPressed:(id)sender
 {
-    // convert yearsToConvertTextField into dog years by multiplying by 7
-    int dogYears = [self.yearsToConvertTextField.text integerValue] * 7;
+    // convert yearsToConvertTextField into dog years
+    // the first years of a dog are equivalent to 10.5 human years
+    // every year after is equivalent to 4 human years
+    int yearsToConvert = [self.yearsToConvertTextField.text integerValue];
+    
+    int dogYears;
+    if (yearsToConvert <= 2)
+        dogYears = yearsToConvert * 10.5;
+    else{
+        dogYears = (yearsToConvert - 2) * 4 + 21;
+    }
     
     // display dogYears in yearsInDogYearsLabel
     self.yearsInDogYearsLabel.text = [NSString stringWithFormat:@"%d dog years!", dogYears];
